b7f1c39cd47c16300bb02c95665c1ca188a61ede
leonwooster <leonwoo@hotmail.com>
2022-07-14 19:26:51 +0800

    added in API project.

--- nest-project1/src/app.controller.ts
+++ nest-project1/src/app.controller.ts
@@ -1,9 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { ObjectSchema } from 'joi';
+import { IsString, IsInt } from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  age: number;
+
+  @IsString()
+  breed: string;
+}
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
